0ea0df0f650a0610fa30995773fe899d646b4102
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-03-31 14:22:26 -0400

    API token for the user

--- app/User.php
+++ app/User.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -25,7 +24,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'api_token',
     ];
 
     /**
