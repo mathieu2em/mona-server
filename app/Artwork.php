0ebde0e504e40601102e70f880360609698b8a39
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-04-02 00:07:45 -0400

    Artwork model

---
+++ app/Artwork.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Grimzy\LaravelMysqlSpatial\Eloquent\SpatialTrait;
+
+class Artwork extends Model
+{
+    use SpatialTrait;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [
+        'id', 'created_at', 'updated_at',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'dimensions' => 'array',
+    ];
+
+    /*
+     * The attributes that are spatial representations.
+     *
+     * @var array
+     */
+    protected $spatialFields = [
+        'location',
+    ];
+
+    /**
+     * Get the artwork's location.
+     *
+     * @param  object  $value
+     * @return object
+     */
+    public function getLocationAttribute($value)
+    {
+        return (object) [
+            'lat' => $value->getLat(),
+            'lng' => $value->getLng(),
+        ];
+    }
+
+    /**
+     * Get the category that owns the artwork.
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Artwork\Category');
+    }
+
+    /**
+     * Get the subcategory that owns the artwork.
+     */
+    public function subcategory()
+    {
+        return $this->belongsTo('App\Artwork\Subcategory');
+    }
+
+    /**
+     * Get the borough that owns the artwork.
+     */
+    public function borough()
+    {
+        return $this->belongsTo('App\Artwork\Borough');
+    }
+}
---
+++ database/migrations/2019_04_02_033545_create_artworks_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArtworksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artworks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->date('produced_at')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('subcategory_id');
+            $table->json('dimensions');
+            $table->unsignedBigInteger('borough_id');
+            $table->point('location');
+            $table->timestamps();
+
+            $table->foreign('category_id')
+                  ->references('id')->on('categories');
+            $table->foreign('subcategory_id')
+                  ->references('id')->on('subcategories');
+            $table->foreign('borough_id')
+                  ->references('id')->on('boroughs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artworks');
+    }
+}
