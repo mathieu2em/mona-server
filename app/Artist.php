f4d5fb94bf03c9f55160b3a6d1b0182538324931
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-04-02 00:57:53 -0400

    Artist model

---
+++ app/Artist.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Artist extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'collective',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at', 'updated_at', 'pivot',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'collective' => 'boolean',
+    ];
+}
