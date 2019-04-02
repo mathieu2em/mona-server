72324ffeb03e4fc6ece9961f3cc81c883d9c79b0
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-04-01 23:28:46 -0400

    Material and technique models for artwork

---
+++ app/Artwork/Material.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Artwork;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Material extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'fr', 'en',
+    ];
+
+    /**
+     * The attributes that should be visible in arrays.
+     *
+     * @var array
+     */
+    protected $visible = [
+        'fr', 'en',
+    ];
+}
