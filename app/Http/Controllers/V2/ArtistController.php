258892c064249e4e74799f6b0712e7a0dac11694
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-04-28 15:06:41 -0400

    Use JSON:API convention for artist endpoint

--- app/Http/Controllers/V2/ArtistController.php
+++ app/Http/Controllers/V2/ArtistController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\V2;
 
 use App\Artist;
+use App\Http\Resources\V2\Artist as ArtistResource;
 use App\Http\Controllers\Controller;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Cache;
 
 class ArtistController extends Controller
@@ -12,23 +13,26 @@ class ArtistController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return Artist[]|Collection
+     * @return AnonymousResourceCollection
      */
     public function index()
     {
-        return Cache::remember('artists', now()->addMonths(1), function () {
-            return Artist::all();
-        });
+        $artists = Cache::remember('artists', now()->addMonths(1),
+            function () {
+                return Artist::all();
+            });
+
+        return ArtistResource::collection($artists);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  Artist  $artist
-     * @return Artist
+     * @return ArtistResource
      */
     public function show(Artist $artist)
     {
-        return $artist;
+        return new ArtistResource($artist);
     }
 }
