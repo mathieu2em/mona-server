eb81c4684d076e06324e6ff010e4a5a4eec7da6d
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-08-08 20:29:55 -0400

    Adding pagination for artwork and artist lists
    Version 2 of the API only

--- app/Http/Controllers/V2/ArtistController.php
+++ app/Http/Controllers/V2/ArtistController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\V2;
 use App\Artist;
 use App\Http\Resources\V2\Artist as ArtistResource;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Cache;
 
@@ -13,13 +14,15 @@ class ArtistController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @param  Request  $request
      * @return AnonymousResourceCollection
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Cache::remember('v2.artists', now()->addMonths(1),
+        $page = $request->input('page', 1);
+        return Cache::remember('v2.artists.' . $page, now()->addMonths(1),
             function () {
-                return ArtistResource::collection(Artist::all());
+                return ArtistResource::collection(Artist::paginate(100));
             });
     }
 
--- app/Http/Controllers/V2/ArtworkController.php
+++ app/Http/Controllers/V2/ArtworkController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\V2;
 use App\Artwork;
 use App\Http\Resources\V2\Artwork as ArtworkResource;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Cache;
 
@@ -13,13 +14,15 @@ class ArtworkController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @param  Request  $request
      * @return AnonymousResourceCollection
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Cache::remember('v2.artworks', now()->addMonths(1),
+        $page = $request->input('page', 1);
+        return Cache::remember('v2.artworks.' . $page, now()->addMonths(1),
             function () {
-                return ArtworkResource::collection(Artwork::all());
+                return ArtworkResource::collection(Artwork::paginate(100));
             });
     }
 
