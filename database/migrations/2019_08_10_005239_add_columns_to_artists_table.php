12155ccfdd6ef54ac305a5b1d9dfef2b30ebc4cf
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-08-09 21:54:49 -0400

    Adding consent and edited columns to artists

---
+++ database/migrations/2019_08_10_005239_add_columns_to_artists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToArtistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('artists', function (Blueprint $table) {
+            $table->boolean('consent')->default(false);
+            $table->boolean('edited')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('artists', function (Blueprint $table) {
+            $table->dropColumn('edited');
+            $table->dropColumn('consent');
+        });
+    }
+}
