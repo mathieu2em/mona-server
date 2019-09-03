735d3d1e2f0fb89088e734891a77e4b17f7e01b1
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-09-03 10:11:26 -0400

    Manual review of ImportMural's results

--- app/Jobs/ImportMural.php
+++ app/Jobs/ImportMural.php
@@ -19,9 +19,6 @@ class ImportMural implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    private $urlFormat;
-    private $progressBar;
-
     /**
      * Create a new job instance.
      *
@@ -33,6 +30,90 @@ class ImportMural implements ShouldQueue
             'dataset/%s/resource/%s/download';
 
         $this->progressBar = new ProgressBar(new ConsoleOutput());
+
+        $this->artists = [
+            'René-Pierre Beaudry (Arpi)'  => ['René-Pierre Beaudry', 'Arpi'],
+            'Mathieu Bories (Mateo)'      => ['Mathieu Bories', 'Mateo'],
+            'Monk-E'                      => ['', 'Monk.E'],
+            'Monk-e'                      => ['', 'Monk.E'],
+            'A\'Shop'                     => ['Ashop'],
+            'Simon Bachand (Stare)'       => ['Simon Bachand', 'Stare'],
+            'Maxime Fortin (4-TIN)'       => ['Maxime Fortin', '4-Tin'],
+            'Dodo'                        => ['Doryan Rabilloud', 'Dodo Ose'],
+            'Dodo Ose'                    => ['Doryan Rabilloud', 'Dodo Ose'],
+            'Dodo Ose)'                   => ['Doryan Rabilloud', 'Dodo Ose'],
+            'Zek'                         => ['', 'Zek One'],
+            'Zek-One'                     => ['', 'Zek One'],
+            'Zek One'                     => ['', 'Zek One'],
+            'Astro'                       => ['', 'Astro'],
+            'Scribe CSX'                  => ['', 'Scribe CSX'],
+            'Omen'                        => ['', 'Omen'],
+            'Labrona'                     => ['', 'Labrona'],
+            'Nico'                        => ['', 'Nico'],
+            'Fluke'                       => ['', 'Fluke'],
+            'Shadow'                      => ['', 'Shadow'],
+            'Smoky'                       => ['', 'Smoky'],
+            'Zilon'                       => ['', 'Zilon Lazer'],
+            'Shalak'                      => ['', 'Shalak Attack'],
+            'Curiot'                      => ['Favio Martinez', 'Curiot Tlalpazotl'],
+            'Rouks'                       => ['', 'ROUKS ONE'],
+            'Miss Teri'                   => ['', 'Miss Teri'],
+            'Fonki'                       => ['', 'FONKi'],
+            'Fleo'                        => ['Rémi Fléo', 'Fléo'],
+            'Rémi Fléo'                   => ['Rémi Fléo', 'Fléo'],
+            'MC Baldassari'               => ['Marie-Clémentine Baldassari', 'MC Baldassari'],
+            'Strike'                      => ['Matt Desilets', 'Striker'],
+            'Flying Eric'                 => ['Eric Dufour', 'Flying Eric'],
+            'Les Hommes de lettres'       => ['Les Hommes de Lettres'],
+            'Ankh One'                    => ['', 'Ankhone'],
+            'Collectif Au pied du mur'    => ['Au pied du mur'],
+            'Miles "El Mac" Gregor'       => ['Miles Gregor', 'El Mac'],
+            'El Mac'                      => ['Miles Gregor', 'El Mac'],
+            'Doryan Rabilloud (Dodo Ose)' => ['Doryan Rabilloud', 'Dodo Ose'],
+            'Five Eight'                  => ['', 'Five8'],
+            'Five 8'                      => ['', 'Five8'],
+            'Five8'                       => ['', 'Five8'],
+            'Dre'                         => ['', 'Earth Crusher'],
+            'Osti one'                    => ['', 'Osti One'],
+            'Esprit'                      => ['', 'Esprit'],
+            'Pantonio'                    => ['Antonio Correia', 'Pantónio'],
+            'Matéo'                       => ['Mathieu Bories', 'Mateo'],
+            'Mateo'                       => ['Mathieu Bories', 'Mateo'],
+            'Mr Crocks'                   => ['Jimmy Crockett', 'Mr.Crocks'],
+            'Cheap Art Collective'        => ['The Cheap Art Collective'],
+            'EL MAC'                      => ['Miles Gregor', 'El Mac'],
+            'GENE PENDON'                 => ['Gene Pendon'],
+            'FRANCIS MONTILLAUD'          => ['Francis Montillaud'],
+            'CYRIELLE TREMBLAY'           => ['Cyrielle Tremblay'],
+            'CARLOS OLIVA'                => ['Carlos Oliva'],
+            'LSNR'                        => ['Lucas Saenger', 'LsnrOne'],
+            'Haks 180'                    => [''],
+            'les élèves'                  => [''],
+            'VHILS'                       => ['Alexandre Manuel Dias Farto', 'Vhils'],
+            'PONI'                        => ['Hilda Palafox', 'Poni'],
+            'Hans Schmitter'              => ['Hans Schmitter', 'HAKS 180'],
+            'SBU'                         => ['', 'SBU One'],
+            'Atelier Nayan'               => ['Nayan'],
+            'citoyenNes'                  => [''],
+            'NAIMO'                       => ['Naïmo Dupéré', 'Naimo'],
+            'Naimo'                       => ['Naïmo Dupéré', 'Naimo'],
+            'MILLO'                       => ['', 'Millo'],
+            'Benny Wliding'               => ['Benny Wilding'],
+            'Stack'                       => ['', 'Stack'],
+            'Mastrocola'                  => ['Philippe Mastrocola'],
+            'Monk.E'                      => ['', 'Monk.E'],
+            '1010'                        => ['', '1010'],
+            'Nelio'                       => ['', 'Nelio'],
+            'Earth Crusher'               => ['', 'Earth Crusher'],
+            'Monosourcil'                 => ['Maxilie Martel', 'Mono Sourcil'],
+            'Axe Lalime'                  => ['', 'Axe Lalime'],
+            'Ella'                        => ['Ella & Pitr'],
+            'Pitr'                        => ['Ella & Pitr'],
+            'Snikr'                       => ['', 'Snikr'],
+            'Axe'                         => ['', 'Axe Lalime'],
+            'Lorem Ipsum'                 => ['', 'Lorem Ipsum'],
+            'Case Maclaim'                => ['Andreas von Chrzanowski', 'Case Maclaim'],
+        ];
     }
 
     /**
@@ -42,101 +123,89 @@ class ImportMural implements ShouldQueue
      */
     public function handle()
     {
-        $category = Artwork\Category::where('fr', 'Murales')->first();
+        $dataset  = '53d2e586-6e7f-4eae-89a1-2cfa7fc29fa0';
+        $resource = 'd325352b-1c06-4c3a-bf5e-1e4c98e0636b';
+
+        $json = json_decode(file_get_contents(sprintf(
+            $this->urlFormat, $dataset, $resource
+        )))->features;
+        $this->progressBar->setMaxSteps(count($json));
+
+        $category = Artwork\Category::where(
+            'fr', 'Murales'
+        )->first();
         $collection = Artwork\Collection::where(
             'name', 'Murales subventionnées, Ville de Montréal'
         )->first();
 
-        $json = json_decode(file_get_contents(sprintf($this->urlFormat,
-            '53d2e586-6e7f-4eae-89a1-2cfa7fc29fa0',
-            'd325352b-1c06-4c3a-bf5e-1e4c98e0636b')));
-        $this->progressBar->setMaxSteps(count($json->features));
-
         $this->progressBar->start();
-        foreach ($json->features as $feature) {
-            $mural = $feature->properties;
-            if (strpos($mural->artiste, 'Cette murale') !== false) {
+        foreach ($json as $artwork) {
+            $artwork = $artwork->properties;
+            if (strpos($artwork->artiste, 'Cette murale') !== false) {
                 continue;
             }
 
-            $produced_at = date_create_from_format('Y', $mural->annee);
+            $produced_at = date_create_from_format('Y-m-d', "$artwork->annee-01-01"); // XXX
 
-            $location = new Point($mural->latitude, $mural->longitude);
+            $location = new Point($artwork->latitude, $artwork->longitude);
 
             $borough = Artwork\Borough::contains('area', $location)->first();
 
-            $model = Artwork::updateOrCreate(
-                ['category_id' => $category->id, 'borough_id' => $borough->id],
-                ['produced_at' => $produced_at, 'location' => $location,
-                 'collection_id' => $collection->id]
-            );
+            $details = trim($artwork->adresse);
 
-            $artists = preg_split('/,|:|&| et /i', preg_replace('/\./', '', $mural->artiste));
-            if ($artists == ["A'SHOP-ANKHONE"]) {
-                $artists = ["AShop", 'Ankh One'];
-            } else if ($artists == ['Snikr - Style Over Status']) {
-                $artists = ['Snikr', 'Style Over Status'];
-            } else if ($artists == ["AShop (Zek", 'Dodo Ose)']) {
-                $artists = ["AShop", 'Zek', 'Dodo Ose'];
+            $model = Artwork::equals('location', $location)->where('category_id', $category->id);
+            if ($model->count() > 1) {
+                error_log('Duplicate found: ' . $details); // XXX
+                continue;
             }
 
+            if ($model = $model->first()) {
+                $model->update(
+                    ['produced_at' => $produced_at, 'category_id' => $category->id,
+                     'borough_id' => $borough->id, 'location' => $location,
+                     'details' => $details, 'collection_id' => $collection->id]
+                );
+            } else {
+                $model = Artwork::create(
+                    ['produced_at' => $produced_at, 'category_id' => $category->id,
+                     'borough_id' => $borough->id, 'location' => $location,
+                     'details' => $details, 'collection_id' => $collection->id]
+                );
+            }
+
+            $artists = preg_split('/,|:|&| - | et /i', preg_replace('/\.$/', '', $artwork->artiste));
+            if ($artists == ['A\'SHOP-ANKHONE']) {
+                $artists = ['Ashop', 'Ankh One'];
+            } else if ($artists == ['A\'Shop (Zek']) {
+                $artists = ['Ashop', 'Zek'];
+            }
             foreach ($artists as $artist) {
-                $name = $this->normalize($artist);
-                if (strpos($name, 'citoyenNes') !== false ||
-                    strpos($name, 'les élèves') !== false) {
-                    continue;
+                $artist = trim($artist);
+                $name = trim($this->artists[$artist][0] ?? $artist);
+                $alias = $this->artists[$artist][1] ?? null;
+
+                if ($alias) {
+                    $model->artists()->syncWithoutDetaching(Artist::firstOrCreate( // XXX
+                        ['alias' => $alias], ['name' => $name]
+                    )->id);
+                } else if ($name) {
+                    $model->artists()->syncWithoutDetaching(Artist::firstOrCreate( // XXX
+                        ['name' => $name], ['alias' => $alias]
+                    )->id);
                 }
+            }
 
-                $collective = $this->isCollective($name);
-
-                $model->artists()->syncWithoutDetaching(Artist::firstOrCreate( // XXX
-                    ['name' => $name], ['collective' => $collective]
+            /*
+            $org = trim(preg_replace('/\(.*?\)/', '', $artwork->organisation));
+            if ($org) {
+                $model->artists()->syncWithoutDetaching(Artist::updateOrCreate(
+                    ['name' => $org], ['collective' => true]
                 )->id);
             }
-
-            $org = trim(preg_replace('/\(.*?\)/', '', $mural->organisation));
-            $model->artists()->syncWithoutDetaching(Artist::updateOrCreate(
-                ['name' => $org], ['collective' => true]
-            )->id);
+            */
 
             $this->progressBar->advance();
         }
         $this->progressBar->finish();
-    }
-
-    /**
-     * TODO.
-     *
-     * @param  string  $artist
-     * @return bool
-     */
-    public function isCollective($artist)
-    {
-        return $artist == 'Jeunes résidents de Place Normandie' ||
-            $artist == "A'Shop" ||
-            $artist == 'Collectif Au pied du mur' ||
-            $artist == 'Nayan' ||
-            $artist == 'Les Hommes de lettres';
-    }
-
-    /**
-     * TODO.
-     *
-     * @param  string  $artist
-     * @return string
-     */
-    public function normalize($artist)
-    {
-        $artist = trim(preg_replace('/\(.*?\)/', '', $artist));
-        switch ($artist) {
-            case 'NAIMO':
-            case 'VHILS':
-            case 'PONI':
-            case 'FRANCIS MONTILLAUD':
-            case 'CYRIELLE TREMBLAY':
-            case 'GENE PENDON':
-                return ucwords(strtolower($artist));
-        }
-        return $artist;
     }
 }
